6800087f4a7b975032f075fabe1bf9f252ec7cb6
nmaswood <nmaswood@civisanalytics.com>
2019-05-02 20:29:28 -0700

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Main } from './Main';
 
 const App: React.FC = () => {
   return (
---
+++ src/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Main: React.FC = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
